158206a0d944481d92e641895534f0f1c17c1c81
koningDaan <138571679+koningDaan@users.noreply.github.com>
2026-01-24 18:19:00 +0100

    navbar gefixt

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home-page.jsx';
-import Work from './pages/Work-page.jsx';
-import Projects from './pages/Projects-page.jsx';
-import Navbar from './layout/Navbar.jsx';
-import Footer from './layout/Footer.jsx';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home-page.jsx";
+import Work from "./pages/Work-page.jsx";
+import Projects from "./pages/Projects-page.jsx";
+import Navbar from "./layout/navbar.jsx";
+import Footer from "./layout/Footer.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/werk" element={<Work />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/werk" element={<Work />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
       <Footer />
     </>
   );
